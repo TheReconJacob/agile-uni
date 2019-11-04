db90020e922d8c524640ce64d56aaaa371cdd7ef
Emily Hall <emily.hall@sky.uk>
2019-11-04 11:20:49 +0000

    Adding empty divs for layout

--- src/App.js
+++ src/App.js
@@ -8,24 +8,27 @@ import { authProvider } from './authProvider';
 
 function App() {
   return (
-    <AzureAD provider={authProvider} forceLogin={true}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="c-heading-beta">
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </AzureAD>
+    <>
+      <AzureAD provider={authProvider} forceLogin={true}>
+        <div className="o-layout">
+          <div className="o-layout__item" style={{ "height": 100, "textAlign": "center", "backgroundColor": "#ffb5b3" }}>
+            <p>Header</p>
+          </div>
+          <div className="o-layout__item" style={{ "textAlign": "center", "backgroundColor": "#fffab3" }}>
+            <p>Search bar</p>
+          </div>
+          <div className="o-layout__item" style={{ "height": 300, "textAlign": "center", "backgroundColor": "#ccffb3" }}>
+            <p>Image</p>
+          </div>
+          <div className="o-layout__item" style={{ "height": 400, "textAlign": "center", "backgroundColor": "#b3f6ff" }}>
+            <p>Description</p>
+          </div>
+          <div className="o-layout__item" style={{ "textAlign": "center", "backgroundColor": "#e0b5ff" }}>
+            <p>Footer</p>
+          </div>
+        </div>
+      </AzureAD>
+    </>
 
   );
 }
