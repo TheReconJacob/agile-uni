027fda400263e516a74964f1dc842f0e68e3a809
Nasir Bawa <nasir.bawa@sky.uk>
2019-11-05 13:32:30 +0000

    added backend server with a api route that connects to the database

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+  host: `agileuni-db.c0i93hgwoofe.us-east-1.rds.amazonaws.com`,
+  port: '3306',
+  user: 'admin',
+  password: '',
+  database: 'AGILEUNI'
+})
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/test', (req, res) => {
+	connection.query('SELECT * FROM employee', function(err, rows, fields) {
+		if (err) throw err;
+		res.send(rows)
+	})
+})
+
+
+app.listen(port, (err) => {
+	if(err) { console.log(err) };
+	console.log('Listening on port ' + port);
+})
---
+++ doc/adr/0005-web-framework.md
@@ -0,0 +1,19 @@
+# 5. web-framework
+
+Date: 2019-11-05
+
+## Status
+
+Accepted
+
+## Context
+
+Deciding which web framework to use with node.js
+
+## Decision
+
+We have chosen Express due to team members experience with it. Also it is very popular and apparently the choice doesn't matter too much
+
+## Consequences
+
+Reduces boilerplate. 
