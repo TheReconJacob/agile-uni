9e13ec86f10243f6535e70f0d2acc0a4b567e50a
Nasir Bawa <nasir.bawa@sky.uk>
2019-11-06 10:55:36 +0000

    removed old tests

--- backend/test/test-pages.js
+++ backend/test/test-pages.js
@@ -1,15 +1,6 @@
 var expect  = require('chai').expect;
 var request = require('request');
 const mysql = require('mysql')
-//var connection = require('connection');
-
-// const connection = mysql.createConnection({
-//     host: `agileuni-db.c0i93hgwoofe.us-east-1.rds.amazonaws.com`,
-//     port: '3306',
-//     user: 'admin',
-//     password: 'agileuniversity',
-//     database: 'AGILEUNI'
-//   })
 
 it('Main page content', function(done) {
     request('http://localhost:5000' , function(error, response, body) {
@@ -18,32 +9,6 @@ it('Main page content', function(done) {
     });
 });
 
-
-// it('connection.connect should ...', function(done) {
-//     connection.connect(function(err, result) {
-//         if(err){
-//             done(err);
-//             return;
-//         }
-//         expect(result).to.equal("SQL CONNECT SUCCESSFUL.");
-//         done();
-//     });
-// });
-
-// describe('Access to DB', function(){
-//     describe('eh?', function(){
-//          it('should connect', function(done){
-//              var connection = mysql.createConnection({
-//                 host: `agileuni-db.c0i93hgwoofe.us-east-1.rds.amazonaws.com`,
-//                 port: '3306',
-//                 user: 'admin',
-//                 password: 'agileuniversity',
-//                 database: 'AGILEUNI'
-//              });
-//              connection.connect(done);
-//          });
-//      })
-//  });
 
 // How to test the json
 it('Employees request columns', function(done) {
