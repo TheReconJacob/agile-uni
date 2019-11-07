a370bd196f4895e3bfa115f1dee26aa9c851a846
skerry1 <sheila.kerry@sky.uk>
2019-11-07 14:25:24 +0000

    cropped width of search bar and text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,31 +7,28 @@ import "../styles/home.scss";
 
 function App() {
   return (
-    <div class="global-border-width">
-      <>
+    <>
+      <div class="o-container o-container--ee">
         <SearchBar />
-        <div
-          className="o-layout o-layout--spaced"
-          style={{ marginTop: 40, padding: 0 }}
-        >
-          <div className="o-layout__item" style={{ padding: 0 }}>
-            <Hero image={picture} />
-          </div>
-          <div className="o-layout__item" style={{ padding: 60 }}>
-            <h2 className="c-heading-bravo">Welcome to the Agile Uni</h2>
-            <p className="c-text-body">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
+      </div>
+
+      <Hero image={picture} />
+
+      <div class="o-container o-container--ee">
+        <div className="o-layout__item">
+          <h2 className="c-heading-bravo">Welcome to the Agile Uni</h2>
+          <p className="c-text-body">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
         </div>
-      </>
-    </div>
+      </div>
+    </>
   );
 }
 
