1033f5652349ffa6d37f0e1cbb208be805407f4e
LauraCNichols <laura.caldwellnichols@sky.uk>
2019-11-07 15:37:14 +0000

    Extracted and queried JWT

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import logo from './logo.svg';
 import './App.scss';
 import { AzureAD } from 'react-aad-msal';
 import { authProvider } from './authProvider';
+var base64 = require('base-64');
+var utf8 = require('utf8');
 
 
 function App() {
+
+  var encodedData = localStorage.getItem('msal.idtoken');
+  var blocks = encodedData.split('.');
+  var croppedData = blocks[1];
+  var bytes = base64.decode(croppedData);
+  var userInfo = utf8.decode(bytes);
+  //console.log(userInfo);
+  var parsedInfo = JSON.parse(userInfo);
+  //console.log(parsedInfo);
+  var userRoles = parsedInfo.roles;
+  //console.log(userRoles);
+
   return (
     <AzureAD provider={authProvider} forceLogin={true}>
       <div className="App">
