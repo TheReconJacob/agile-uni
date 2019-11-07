47e236d6c81caa890559e736c0fb49e0347280a7
LauraCNichols <laura.caldwellnichols@sky.uk>
2019-11-07 15:37:14 +0000

    Extracted and queried JWT

--- src/App.js
+++ src/App.js
@@ -7,8 +7,23 @@ import Home from "./pages/Home";
 import Navbar from "./components/Navbar.js"
 import Footer from "./components/Footer.js"
 import { Route, BrowserRouter as Router } from "react-router-dom";
+var base64 = require('base-64');
+var utf8 = require('utf8');
+
 
 function App() {
+
+  var encodedData = localStorage.getItem('msal.idtoken');
+  var blocks = encodedData.split('.');
+  var croppedData = blocks[1];
+  var bytes = base64.decode(croppedData);
+  var userInfo = utf8.decode(bytes);
+  //console.log(userInfo);
+  var parsedInfo = JSON.parse(userInfo);
+  //console.log(parsedInfo);
+  var userRoles = parsedInfo.roles;
+  //console.log(userRoles);
+
   return (
     <>
       <AzureAD provider={authProvider} forceLogin={true}>
