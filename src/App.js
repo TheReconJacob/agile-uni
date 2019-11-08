44f544730ace853ac405173530d194b3bce2ceb8
LauraCNichols <laura.caldwellnichols@sky.uk>
2019-11-08 11:20:43 +0000

    Created admin state

--- src/App.js
+++ src/App.js
@@ -10,47 +10,67 @@ var base64 = require('base-64');
 var utf8 = require('utf8');
 
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  var encodedData = localStorage.getItem('msal.idtoken');
-  var blocks = encodedData.split('.');
-  var croppedData = blocks[1];
-  var bytes = base64.decode(croppedData);
-  var userInfo = utf8.decode(bytes);
-  //console.log(userInfo);
-  var parsedInfo = JSON.parse(userInfo);
-  //console.log(parsedInfo);
-  var userRoles = parsedInfo.roles;
-  //console.log(userRoles);
+    // We can add more state
+    this.state = {
+      admin: false,
+    };
 
-  return (
-    <AzureAD provider={authProvider} forceLogin={true}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="c-heading-beta">
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        {/* <Router>
-          <Switch>
-            <Route path="/ExampleAdmin" allowed={['admin']}>
-              <ExampleAdmin />
-            </Route>
-          </Switch>
-        </Router> */}
-      </div>
-    </AzureAD>
+    var roles = this.getRoles();
+    if(roles.includes('admin')) {
+      this.state.admin = true;
+    }
+    console.log(this.state.admin);
+  }
 
-  );
+  getRoles() {
+    var encodedData = localStorage.getItem('msal.idtoken');
+    var blocks = encodedData.split('.');
+    var croppedData = blocks[1];
+    var bytes = base64.decode(croppedData);
+    var userInfo = utf8.decode(bytes);
+    var parsedInfo = JSON.parse(userInfo);
+    // ['testy', 'admin']
+    var userRoles = parsedInfo.roles;
+    console.log(userRoles);
+    return userRoles;
+  }
+
+
+  render() {
+    return (
+      <AzureAD provider={authProvider} forceLogin={true}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p className="c-heading-beta">
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+          {/* <Router>
+            <Switch>
+              <Route path="/ExampleAdmin" allowed={['admin']}>
+                <ExampleAdmin />
+              </Route>
+            </Switch>
+          </Router> */}
+        </div>
+      </AzureAD>
+  
+    );
+  }
+
 }
 
 export default App;
