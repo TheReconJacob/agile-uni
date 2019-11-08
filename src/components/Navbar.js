0f23c5ae6014b95fcaf06dc4ad663a9347675f33
Nasir Bawa <nasir.bawa@sky.uk>
2019-11-08 13:32:38 +0000

    fixed navbar and changed search bar sizing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,26 +9,29 @@ class Navbar extends React.Component {
       <nav>
         <div className="c-simple-masthead">
           <div className="c-simple-masthead__inner o-container">
-            <div>
-              <img
-                className="c-simple-masthead__logo u-width-1/4"
-                src={agileUniLogo}
-                sizes="100vw"
-              />
-              <span className="c-simple-masthead__title">
-                <div>
-                  <a class="o-layout__item u-width-1/4">
-                    <Link className="navbar-link" to="/">
-                      Home
+            <img
+              className="c-simple-masthead__logo o-layout__item u-width-1/4 navbar-logo"
+              src={agileUniLogo}
+              sizes="100vw"
+            />
+            <div className="c-simple-masthead__title o-layout__item u-width-3/4 navbar-items">
+              <div>
+                <ul className="navbar-link-items">
+                  <li className="navbar-link-item">
+                      <Link className="navbar-link-color c-heading-delta" to="/">
+                        Home
                     </Link>
-                  </a>
-                  <a class="o-layout__item u-width-1/4 ">
-                    <Link className="navbar-link" to="/courses">
-                      Courses
+                  </li>
+                  <li className="navbar-link-item">
+                      <Link className="navbar-link-color c-heading-delta" to="/courses">
+                        Courses
                     </Link>
-                  </a>
-                </div>
-              </span>
+                  </li>
+
+                </ul>
+
+
+              </div>
             </div>
           </div>
         </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -17,46 +17,48 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <form>
-        <fieldset>
-          <ul class="c-form-list o-layout--spaced">
-            <li class="c-form-list">
-              <select
-                id="f-sites"
-                name="site"
-                class="c-form-select__dropdown--inline o-layout__item u-width-1/4"
-                onChange={this.handleChange}
-                style={{ height: 39 }}
-              >
-                <option value="" disabled selected>
-                  Location
+      <div className="o-container">
+        <form>
+          <fieldset>
+            <ul class="c-form-list o-layout--spaced">
+              <li class="c-form-list">
+                <select
+                  id="f-sites"
+                  name="site"
+                  class="c-form-select__dropdown--inline o-layout__item u-width-1/4"
+                  onChange={this.handleChange}
+                  style={{ height: 39 }}
+                >
+                  <option value="" disabled selected>
+                    Location
                 </option>
-                <option value="Osterley">Osterley</option>
-                <option value="Leeds">Leeds</option>
-                <option value="Livingston">Livingston</option>
-              </select>
-              <div class="c-form-combo--inline o-layout__item u-width-3/4">
-                <div class="c-form-combo__cell">
-                  <input
-                    type="text"
-                    name="searchTerm"
-                    class="c-form-combo__input c-form-input"
-                    placeholder="Search for your next course"
-                    name="searchTerm"
-                    id="f-combo"
-                    onChange={this.handleChange}
-                  />
-                </div>
-                <div class="c-form-combo__cell">
-                  <button class="c-form-combo__btn c-btn c-btn--primary">
-                    Search
+                  <option value="Osterley">Osterley</option>
+                  <option value="Leeds">Leeds</option>
+                  <option value="Livingston">Livingston</option>
+                </select>
+                <div class="c-form-combo--inline o-layout__item u-width-3/4">
+                  <div class="c-form-combo__cell">
+                    <input
+                      type="text"
+                      name="searchTerm"
+                      class="c-form-combo__input c-form-input"
+                      placeholder="Search for your next course"
+                      name="searchTerm"
+                      id="f-combo"
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                  <div class="c-form-combo__cell">
+                    <button class="c-form-combo__btn c-btn c-btn--primary">
+                      Search
                   </button>
+                  </div>
                 </div>
-              </div>
-            </li>
-          </ul>
-        </fieldset>
-      </form>
+              </li>
+            </ul>
+          </fieldset>
+        </form>
+      </div>
     );
   }
 }
