cbada17557e3b7239f64d3435f729696dbf7b00b
LauraCNichols <laura.caldwellnichols@sky.uk>
2019-11-08 11:20:43 +0000

    Created admin state

--- src/App.js
+++ src/App.js
@@ -10,36 +10,55 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 var base64 = require('base-64');
 var utf8 = require('utf8');
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-function App() {
+    // We can add more state
+    this.state = {
+      admin: false,
+    };
 
-  var encodedData = localStorage.getItem('msal.idtoken');
-  var blocks = encodedData.split('.');
-  var croppedData = blocks[1];
-  var bytes = base64.decode(croppedData);
-  var userInfo = utf8.decode(bytes);
-  //console.log(userInfo);
-  var parsedInfo = JSON.parse(userInfo);
-  //console.log(parsedInfo);
-  var userRoles = parsedInfo.roles;
-  //console.log(userRoles);
+    var roles = this.getRoles();
+    if (roles.includes('admin')) {
+      this.state.admin = true;
+    }
+    console.log(this.state.admin);
+  }
 
-  return (
-    <>
-      <AzureAD provider={authProvider} forceLogin={true}>
-        <Router>
-          <Navbar />
-          <div>
-            <Route exact path="/" component={Home} />
-            <Route path="/courses" component={Courses} />
-          </div>
-          <Footer />
-        </Router>
-      </AzureAD>
+  getRoles() {
+    var encodedData = localStorage.getItem('msal.idtoken');
+    var blocks = encodedData.split('.');
+    var croppedData = blocks[1];
+    var bytes = base64.decode(croppedData);
+    var userInfo = utf8.decode(bytes);
+    var parsedInfo = JSON.parse(userInfo);
+    // ['testy', 'admin']
+    var userRoles = parsedInfo.roles;
+    console.log(userRoles);
+    return userRoles;
+  }
 
-    </>
-    
-  );
+
+  render() {
+    return (
+      <>
+        <AzureAD provider={authProvider} forceLogin={true}>
+          <Router>
+            <Navbar />
+            <div>
+              <Route exact path="/" component={Home} />
+              <Route path="/courses" component={Courses} />
+            </div>
+            <Footer />
+          </Router>
+        </AzureAD>
+
+      </>
+
+    );
+  }
+
 }
 
 export default App;
