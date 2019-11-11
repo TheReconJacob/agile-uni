c51b3787f07f69aa43274f065252167a1802cf99
doma91 <dominika.malinowska@sky.uk>
2019-11-11 11:58:01 +0000

    cenytring text in from

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "../components/SearchBar";
 import DatePicker from "react-datepicker";
+import "../styles/admin.scss";
 
 class Admin extends React.Component {
     constructor() {
@@ -15,24 +16,24 @@ class Admin extends React.Component {
           <SearchBar />
         </div>
       </div>
-           <div class="o-container o-container--ee u-padding-bottom-large u-padding-top-large">
+           <div class="o-container o-container--ee u-padding-bottom-large u-padding-top-large form-container">
            <div className="o-layout__item">
       <form>
           <fieldset>
             <legend class="c-form-caption">Add courses</legend>
             <ul class="c-form-list">
-                <li class="c-form-list__item">
+                <li class="c-form-list__item u-width-1/2">
                     <input type="text" class="c-form-input" placeholder="Title..." name="f-title" id="f-title" required />
                 </li>
-                <li class="c-form-list__item">
+                <li class="c-form-list__item u-width-1/2">
                     <input type="date" class="c-form-date c-form-combo--inline o-layout__item u-width-3/4 " placeholder="Choose a start date..." name="f-start-date" id="f-start-date" />
                     <input type="time" class="c-form-time c-form-combo--inline o-layout__item u-width-1/4" placeholder="Choose a start time..." name="f-start-time" id="f-start-time" />
                 </li>
-                <li class="c-form-list__item">
+                <li class="c-form-list__item u-width-1/2">
                     <input type="date" class="c-form-date c-form-combo--inline o-layout__item u-width-3/4" placeholder="Choose an end time and date..." name="f-end-date" id="f-end-date" />
                     <input type="time" class="c-form-time c-form-combo--inline o-layout__item u-width-1/4" placeholder="Choose an end time..." name="f-end-time" id="f-end-time" />
                 </li>
-                <li class="c-form-list__item">
+                <li class="c-form-list__item u-width-1/2">
                 <div class="c-form-select">
                     <select id="f-heroes" class="c-form-select__dropdown">
                     <option value="" disabled selected>Choose a location...</option>
@@ -42,11 +43,11 @@ class Admin extends React.Component {
                     </select>
                 </div>
                 </li>
-                <li class="c-form-list__item">
+                <li class="c-form-list__item u-width-1/2">
                     <input type="number" min="0" class="c-form-date" placeholder="Choose number of participants..." name="f-number-of-participants" id="f-number-of-participants" />
                   
                 </li>
-                <li class="c-form-list__item">
+                <li class="c-form-list__item u-width-1/2">
                     <textarea class="c-form-input c-form-input--long" placeholder="Description..." name="f-description" id="f-description"></textarea>
                 </li>
           <button type="submit" className="c-btn c-btn--primary">Add Course</button>
