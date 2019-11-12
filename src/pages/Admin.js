3e670af6b6a9a9447ce2500e160ece28a91ecf71
Rose <47150066+GilEstelsBeams@users.noreply.github.com>
2019-11-12 16:30:01 +0000

    refactoring the admin page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -34,84 +34,84 @@ class Admin extends React.Component {
             </div>
         </div>
       
-        <div class="o-container u-padding-bottom-large u-padding-top-large form-container ">
+        <div className="o-container u-padding-bottom-large u-padding-top-large form-container ">
             <form onSubmit={this.handleSubmit}>
                 <fieldset>
-                    <legend class="c-form-caption">Add courses</legend>
-                    <ul class="c-form-list">
-                        <li class="c-form-list__item u-width-1/2">
-                            <label class="c-form-label " for="title">
+                    <legend className="c-form-caption">Add courses</legend>
+                    <ul className="c-form-list">
+                        <li className="c-form-list__item u-width-1/2">
+                            <label className="c-form-label ">
                                 Title 
-                                <abbr title="This field is required" class="c-form-required">
+                                <abbr title="This field is required" className="c-form-required">
                                     *
                                 </abbr>
                             </label>
                         </li>
-                        <li class="c-form-list__item u-width-1/2">
-                            <input type="text" class="c-form-input" placeholder="Title..." name="f-title" id="f-title" onChange={this.handleChange} required />
+                        <li className="c-form-list__item u-width-1/2">
+                            <input type="text" className="c-form-input" placeholder="Title..." name="f-title" id="f-title" onChange={this.handleChange} required />
                         </li>
-                        <li class="c-form-list__item u-width-1/2">
-                            <label class="c-form-label" for="start-date">
+                        <li className="c-form-list__item u-width-1/2">
+                            <label className="c-form-label">
                                 Start date 
-                                <abbr title="This field is required" class="c-form-required">
+                                <abbr title="This field is required" className="c-form-required">
                                     *
                                 </abbr>
                             </label>
                         </li>
-                        <li class="c-form-list__item u-width-1/2">
-                            <input type="date" class="c-form-date c-form-combo--inline o-layout__item u-width-3/4 " placeholder="Choose a start date..." name="f-start-date" id="f-start-date" onChange={this.handleChange} required />
-                            <input type="time" class="c-form-date c-form-combo--inline o-layout__item u-width-1/4" placeholder="Choose a start time..." name="f-start-time" id="f-start-time" onChange={this.handleChange} required />
+                        <li className="c-form-list__item u-width-1/2">
+                            <input type="date" className="c-form-date c-form-combo--inline o-layout__item u-width-3/4 " placeholder="Choose a start date..." name="f-start-date" id="f-start-date" onChange={this.handleChange} required />
+                            <input type="time" className="c-form-date c-form-combo--inline o-layout__item u-width-1/4" placeholder="Choose a start time..." name="f-start-time" id="f-start-time" onChange={this.handleChange} required />
                         </li>
-                        <li class="c-form-list__item u-width-1/2">
-                            <label class="c-form-label" for="end-date">
+                        <li className="c-form-list__item u-width-1/2">
+                            <label className="c-form-label">
                                 End date 
-                                <abbr title="This field is required" class="c-form-required">
+                                <abbr title="This field is required" className="c-form-required">
                                     *
                                 </abbr>
                             </label>
                         </li>
-                        <li class="c-form-list__item u-width-1/2">
-                            <input type="date" class="c-form-date c-form-combo--inline o-layout__item u-width-3/4" placeholder="Choose an end time and date..." name="f-end-date" id="f-end-date" onChange={this.handleChange} required />
-                            <input type="time" class="c-form-date c-form-combo--inline o-layout__item u-width-1/4" placeholder="Choose an end time..." name="f-end-time" id="f-end-time" onChange={this.handleChange} required />
+                        <li className="c-form-list__item u-width-1/2">
+                            <input type="date" className="c-form-date c-form-combo--inline o-layout__item u-width-3/4" placeholder="Choose an end time and date..." name="f-end-date" id="f-end-date" onChange={this.handleChange} required />
+                            <input type="time" className="c-form-date c-form-combo--inline o-layout__item u-width-1/4" placeholder="Choose an end time..." name="f-end-time" id="f-end-time" onChange={this.handleChange} required />
                         </li>
-                        <li class="c-form-list__item u-width-1/2">
-                            <label class="c-form-label" for="location">
+                        <li className="c-form-list__item u-width-1/2">
+                            <label className="c-form-label">
                                 Location 
-                                <abbr title="This field is required" class="c-form-required">
+                                <abbr title="This field is required" className="c-form-required">
                                     *
                                 </abbr>
                             </label>
                         </li>
-                        <li class="c-form-list__item u-width-1/2">
-                            <div class="c-form-select">
-                                <select id="f-heroes" class="c-form-select__dropdown" onChange={this.handleChange} required>
-                                    <option value="" disabled selected>Choose a location...</option>
+                        <li className="c-form-list__item u-width-1/2">
+                            <div className="c-form-select">
+                                <select id="f-heroes" className="c-form-select__dropdown" onChange={this.handleChange} defaultValue={'DEFAULT'} required>
+                                    <option value="DEFAULT" disabled>Choose a location...</option>
                                     <option value="Osterley">Osterley</option>
                                     <option value="Leeds">Leeds</option>
                                     <option value="Livingston">Livingston</option>
                                 </select>
                             </div>
                         </li>
-                        <li class="c-form-list__item u-width-1/2">
-                            <label class="c-form-label" for="number-participants">
+                        <li className="c-form-list__item u-width-1/2">
+                            <label className="c-form-label">
                                 Maximum number of participants 
-                                <abbr title="This field is required" class="c-form-required">
+                                <abbr title="This field is required" className="c-form-required">
                                     *
                                 </abbr>
                             </label>
                         </li>
-                        <li class="c-form-list__item u-width-1/2">
-                            <input type="number" min="0" class="c-form-date" placeholder="Choose maximum number of participants..." name="f-number-of-participants" id="f-number-of-participants" onChange={this.handleChange} required/>
+                        <li className="c-form-list__item u-width-1/2">
+                            <input type="number" min="0" className="c-form-date" placeholder="Choose maximum number of participants..." name="f-number-of-participants" id="f-number-of-participants" onChange={this.handleChange} required/>
                         </li>
-                        <li class="c-form-list__item u-width-1/2">
-                            <label class="c-form-label" for="description">
+                        <li className="c-form-list__item u-width-1/2">
+                            <label className="c-form-label">
                                 Description 
-                                <abbr title="This field is required" class="c-form-required">
+                                <abbr title="This field is required" className="c-form-required">
                                     *
                                 </abbr>
                             </label>
                         </li>
-                        <li class="c-form-list__item u-width-1/2">
+                        <li className="c-form-list__item u-width-1/2">
                             <Quill />
                         </li>
                         <button type="submit" className="c-btn c-btn--primary">Add Course</button>
