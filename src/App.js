d5e609dd1d921870634b684e27032640c1381113
Nasir Bawa <nasir.bawa@sky.uk>
2019-11-13 14:32:52 +0000

    logging signing key and jwks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.scss";
-import { AzureAD} from "react-aad-msal";
+import { AzureAD } from "react-aad-msal";
 import { authProvider } from "./authProvider";
 import Courses from "./pages/Courses";
 import Home from "./pages/Home";
@@ -8,28 +8,29 @@ import Navbar from "./components/Navbar.js"
 import Footer from "./components/Footer.js"
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Admin from "./pages/Admin";
+
 var base64 = require('base-64');
 var utf8 = require('utf8');
+const oicdUrl = "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration"
 
-  // trying new method here
-  
-  const request = async url => {
-    const token = await authProvider.getAccessToken();
-  
-    return fetch(url, {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + token.accessToken,
-        'Content-Type': 'application/json',
-      },
-    });
-  };
-  // end of new method
+// trying new method here
+
+// const request = async url => {
+//   const token = await authProvider.getAccessToken();
+
+//   return fetch(url, {
+//     method: 'GET',
+//     headers: {
+//       Authorization: 'Bearer ' + token.accessToken,
+//       'Content-Type': 'application/json',
+//     },
+//   });
+// };
+// end of new method
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     // We can add more state
     this.state = {
       admin: false,
@@ -45,6 +46,54 @@ class App extends React.Component {
       console.log("This is admin boolean " + this.state.admin);
     }
   }
+
+  async getSigningKey() {
+    const token = await authProvider.getAccessToken()
+    console.log(token);
+  }
+
+  async getJwksUrl() {
+
+    //do error handling
+    const oicdRes = await fetch(oicdUrl)
+    const oicdJson = await oicdRes.json()
+    const jwksUri = oicdJson.jwks_uri
+    const jwksRes = await fetch(jwksUri)
+    const jwksJson = await jwksRes.json()
+    console.log(jwksJson.keys)
+
+    // fetch(oicdUrl)
+    // .then(response => response.json())
+    // .then(data => {
+    //   const jwksUri = data.jwks_uri
+    //   return fetch(jwksUri)
+    // })
+    // .then(response => response.json())    
+    // .then(data => {
+    //   if (!data || !data.keys.length) {
+    //     return console.error("JWKS endpoint doesn't contain keys check OICD url")
+    //   }
+    //   console.log(data.keys)
+    //   return data.keys
+    // })
+    // .catch(error => console.error(error))
+  }
+
+  // verifyAccessToken() {
+  //   const token = await authProvider.getAccessToken();
+  //   const token = token.accessToken;
+  //   var x5cString = 'MIIDBTCCAe2gAwIBAgIQaD0+a2FNl5JMaE2/7KUP5zANBgkqhkiG9w0BAQsFADAtMSswKQYDVQQDEyJhY2NvdW50cy5hY2Nlc3Njb250cm9sLndpbmRvd3MubmV0MB4XDTE3MTEyODAwMDAwMFoXDTE5MTEyOTAwMDAwMFowLTErMCkGA1UEAxMiYWNjb3VudHMuYWNjZXNzY29udHJvbC53aW5kb3dzLm5ldDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKdx4TELKpoEo3og7GdWsvlABYBTHQWIBhpBmti9HsOv6t29sfQnHa7v2pladR11dhVcG6StB2LE0kb1iIgkUU/gIDEiJWqe6pYPPomL3RSCbKK3/OPrETfSKtxdaSg/lvb6rIrXBG85Yxwi0ktr1Ckm+4rcI3PwCo5Mt3F+Z3TLqUBEmTU24sGpzPXQCeyvO6VEVzeupUv6vbu8MVr43N4ixKduT1P9OpqBgzHuQVrg+kW/6XRTNaZZthTlITG8VEa8I02S2Q4VyosdzcrMQmRp75MTA2YKdqTexTKmWKaXFIcGCbvMEm35sIfIVziQ8YMSEC+7GBoh3k2g2BFRgtECAwEAAaMhMB8wHQYDVR0OBBYEFGpho/kx4FeBzTTBIbYVWfFdGIblMA0GCSqGSIb3DQEBCwUAA4IBAQBgaeYvuV4sYJZNPXbxN6FIc0oA4bCg1+DFalT3qzW0bFbl7pXP5YPY/adZlT8+cTznjMgzPPvcXaQdcytR16YqHJVX0ik67NrrQ3g0TtGLc42SuzE+6awryiLQY0Zd799bJwrRcDZUqm/6jKOEOd+utf+3o+VU57w+n7iQLuSL1mKYGMsyzMYCj6xVtYl75iam86leT0/tlXHJ8oqxU5w3xVY6P7v0Sz2UteQNQvhKi6R1/cjfiJUp+OzHpaEor6ls7HSvojY2zm1DIaH6EBp2L9TBFt8IALAEN5hX6XYpKmCHRUlzYwoWo7YwzKiot0XdbDW+zPKGPKtIlbg6cw/+';
+  //   var publicKey = '-----BEGIN CERTIFICATE-----\n' + x5cString + '\n-----END CERTIFICATE-----';
+  //   var verifyOptions = {
+  //     // algorithms: ['RS256'],
+  //     // audience: '<Your audience (aud) GUID>',
+  //     // issuer: '<Your issuer (iss) value>'
+  //   };
+
+  //   // Verify
+  //   var verifiedToken = jwt.verify(token, publicKey) //, verifyOptions);
+  //   console.log(verifiedToken); ˇ
+  // }
 
   getRoles() {
     let userRoles = [];
@@ -65,7 +114,9 @@ class App extends React.Component {
   render() {
     let adminComponent;
     let adminAddCourse;
-    var adminS  = this.state.admin;
+    var adminS = this.state.admin;
+    this.getJwksUrl()
+    this.getSigningKey()
     if (this.state.admin) {
       adminComponent = <h1>HELLO ADMIN</h1>;
       adminAddCourse = <Route path="/admin" component={Admin} />;
@@ -77,7 +128,7 @@ class App extends React.Component {
             <Navbar />
             <div>
               <Route exact path="/" component={Home} />
-              <Route path="/courses" render={()=><Courses adminStatus={adminS}/>}/>
+              <Route path="/courses" render={() => <Courses adminStatus={adminS} />} />
               {adminAddCourse}
             </div>
             {adminComponent}
