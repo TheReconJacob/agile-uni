296f2e5a94681a4a04dd749e2e37fb05567b7b41
Nasir Bawa <nasir.bawa@sky.uk>
2019-11-14 16:15:44 +0000

    added backend authentication for routes. You'll need to generate a access_token to test any routes... or comment it out while developing

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,20 @@ const app = express();
 const port = 5000;
 const mysql = require('mysql')
 const fs = require('fs')
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 
 const connection = mysql.createConnection({
-  host: `agileuni-db.c0i93hgwoofe.us-east-1.rds.amazonaws.com`,
-  port: '3306',
-  user: 'admin',
-  password: fs.readFileSync('../dbPassword', 'UTF-8'),
-  database: 'AGILEUNI'
+	host: `agileuni-db.c0i93hgwoofe.us-east-1.rds.amazonaws.com`,
+	port: '3306',
+	user: 'admin',
+	password: fs.readFileSync('../dbPassword', 'UTF-8'),
+	database: 'AGILEUNI'
 })
 
-connection.connect(function(err) {
-    if (err) {
+connection.connect(function (err) {
+	if (err) {
 		throw err;
 	} else {
 		console.log('db connection successful')
@@ -23,6 +25,20 @@ connection.connect(function(err) {
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(jwt({
+	// Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint.
+	secret: jwksRsa.expressJwtSecret({
+	  cache: true,
+	  rateLimit: true,
+	  jwksRequestsPerMinute: 5,
+	  jwksUri: `https://login.microsoftonline.com/common/discovery/v2.0/keys`
+	}),
+  
+	// Validate the audience and the issuer.
+	audience: 'c0fb79ba-b72c-47c1-912c-48ee6cbac972',
+	issuer: 'https://login.microsoftonline.com/68b865d5-cf18-4b2b-82a4-a4eddb9c5237/v2.0',
+	algorithms: [ 'RS256' ]
+  }));
 
 app.get('/', (req, res) => {
 	res.send('Hello World')
@@ -30,7 +46,7 @@ app.get('/', (req, res) => {
 
 //'SELECT * FROM users WHERE id = ?', [userId]
 app.get('/employees', (req, res) => {
-	connection.query('SELECT * FROM employees LIMIT 1', function(err, rows, fields) {
+	connection.query('SELECT * FROM employees LIMIT 1', function (err, rows, fields) {
 		if (err) throw err;
 		console.log(rows);
 		res.send(rows)
@@ -38,14 +54,14 @@ app.get('/employees', (req, res) => {
 })
 
 app.get('/courses', (req, res) => {
-	connection.query('SELECT * FROM courses LIMIT 1', function(err, rows, fields) {
+	connection.query('SELECT * FROM courses LIMIT 1', function (err, rows, fields) {
 		if (err) throw err;
 		res.send(rows)
 	})
 })
 
 app.get('/sites', (req, res) => {
-	connection.query('SELECT * FROM sites LIMIT 1', function(err, rows, fields) {
+	connection.query('SELECT * FROM sites LIMIT 1', function (err, rows, fields) {
 		if (err) throw err;
 		console.log(rows);
 		res.send(rows)
@@ -54,7 +70,7 @@ app.get('/sites', (req, res) => {
 
 
 app.listen(port, (err) => {
-	if(err) { console.log(err) };
+	if (err) { console.log(err) };
 	console.log('Listening on port ' + port);
 })
 
