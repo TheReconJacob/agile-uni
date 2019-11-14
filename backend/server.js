b7965803c640a4b1da5c4b7f213a754c43704e40
Nasir Bawa <nasir.bawa@sky.uk>
2019-11-14 16:15:44 +0000

    added backend authentication for routes. You'll need to generate a access_token to test any routes... or comment it out while developing

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const mysql = require('mysql')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //app.use('/search', searchRoutes);
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 const Data = require("./data/data_access.js");
 const dataHandler = require("./data/dataHandler.js");
@@ -84,3 +86,21 @@ module.exports.SimpleMessage = "Hello world";
 module.exports.closeServer = function() {
   server.close();
 };
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(jwt({
+	// Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint.
+	secret: jwksRsa.expressJwtSecret({
+	  cache: true,
+	  rateLimit: true,
+	  jwksRequestsPerMinute: 5,
+	  jwksUri: `https://login.microsoftonline.com/common/discovery/v2.0/keys`
+	}),
+  
+	// Validate the audience and the issuer.
+	audience: 'c0fb79ba-b72c-47c1-912c-48ee6cbac972',
+	issuer: 'https://login.microsoftonline.com/68b865d5-cf18-4b2b-82a4-a4eddb9c5237/v2.0',
+	algorithms: [ 'RS256' ]
+  }));
--- src/App.js
+++ src/App.js
@@ -9,26 +9,7 @@ import Footer from "./components/Footer.js"
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Admin from "./pages/Admin";
 
-// var jwt = require('jsonwebtoken');
-// const jwksClient = require('jwks-rsa')
-var base64 = require('base-64');
-var utf8 = require('utf8');
-const oicdUrl = "https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration"
-
-// trying new method here
-
-// const request = async url => {
-//   const token = await authProvider.getAccessToken();
-
-//   return fetch(url, {
-//     method: 'GET',
-//     headers: {
-//       Authorization: 'Bearer ' + token.accessToken,
-//       'Content-Type': 'application/json',
-//     },
-//   });
-// };
-// end of new method
+const jwt = require('jsonwebtoken');
 
 class App extends React.Component {
   constructor(props) {
@@ -38,89 +19,32 @@ class App extends React.Component {
       admin: false,
     };
 
-    if (localStorage.getItem('msal.idtoken') == null) {
-      console.log("Not logged in");
-    } else {
-      var roles = this.getRoles();
-      if (roles.includes('admin')) {
-        this.state.admin = true;
+    this.getRoles().then(roles => {
+      if (roles.includes('admin')){
+        this.setState({
+          admin: true
+        });
       }
-      console.log("This is admin boolean " + this.state.admin);
+    });
+  }
+
+  async getRoles(){
+    const token = await authProvider.getIdToken();
+    const idToken = token.idToken.rawIdToken;
+    const decodedToken = jwt.decode(idToken);
+
+    if (idToken != null) {
+      console.log('Returning roles');
+      return decodedToken.roles;
+    } else {
+      console.log('Failed to obtain token')
     }
-  }
-
-  async getSigningKey() {
-    const token = await authProvider.getAccessToken()
-    console.log(token.accessToken);
-
-    // const decodedToken = jwt.decode(token.scopes)
-    // const kid = decodedToken.kid
-    // console.log(decodedToken);
-  }
-
-  async getIdToken() {
-    const token = await authProvider.getIdToken()
-    console.log(token)
-    // const decodedToken = jwt.decode(token)
-    // const kid = decodedToken.kid
-    // console.log(decodedToken);
-  }
-
-
-
-  // async getJwksUrl() {
-
-  //   //do error handling
-  //   const oicdRes = await fetch(oicdUrl)
-  //   const oicdJson = await oicdRes.json()
-  //   return jwksUri = oicdJson.jwks_uri
-  //   // const jwksRes = await fetch(jwksUri)
-  //   // const jwksJson = await jwksRes.json()
-  //   // console.log(jwksJson.keys)
-
-  // }
-
-
-
-  // verifyAccessToken() {
-  //   const token = await authProvider.getAccessToken();
-  //   const token = token.accessToken;
-  //   var x5cString = 'MIIDBTCCAe2gAwIBAgIQaD0+a2FNl5JMaE2/7KUP5zANBgkqhkiG9w0BAQsFADAtMSswKQYDVQQDEyJhY2NvdW50cy5hY2Nlc3Njb250cm9sLndpbmRvd3MubmV0MB4XDTE3MTEyODAwMDAwMFoXDTE5MTEyOTAwMDAwMFowLTErMCkGA1UEAxMiYWNjb3VudHMuYWNjZXNzY29udHJvbC53aW5kb3dzLm5ldDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKdx4TELKpoEo3og7GdWsvlABYBTHQWIBhpBmti9HsOv6t29sfQnHa7v2pladR11dhVcG6StB2LE0kb1iIgkUU/gIDEiJWqe6pYPPomL3RSCbKK3/OPrETfSKtxdaSg/lvb6rIrXBG85Yxwi0ktr1Ckm+4rcI3PwCo5Mt3F+Z3TLqUBEmTU24sGpzPXQCeyvO6VEVzeupUv6vbu8MVr43N4ixKduT1P9OpqBgzHuQVrg+kW/6XRTNaZZthTlITG8VEa8I02S2Q4VyosdzcrMQmRp75MTA2YKdqTexTKmWKaXFIcGCbvMEm35sIfIVziQ8YMSEC+7GBoh3k2g2BFRgtECAwEAAaMhMB8wHQYDVR0OBBYEFGpho/kx4FeBzTTBIbYVWfFdGIblMA0GCSqGSIb3DQEBCwUAA4IBAQBgaeYvuV4sYJZNPXbxN6FIc0oA4bCg1+DFalT3qzW0bFbl7pXP5YPY/adZlT8+cTznjMgzPPvcXaQdcytR16YqHJVX0ik67NrrQ3g0TtGLc42SuzE+6awryiLQY0Zd799bJwrRcDZUqm/6jKOEOd+utf+3o+VU57w+n7iQLuSL1mKYGMsyzMYCj6xVtYl75iam86leT0/tlXHJ8oqxU5w3xVY6P7v0Sz2UteQNQvhKi6R1/cjfiJUp+OzHpaEor6ls7HSvojY2zm1DIaH6EBp2L9TBFt8IALAEN5hX6XYpKmCHRUlzYwoWo7YwzKiot0XdbDW+zPKGPKtIlbg6cw/+';
-  //   var publicKey = '-----BEGIN CERTIFICATE-----\n' + x5cString + '\n-----END CERTIFICATE-----';
-  //   var verifyOptions = {
-  //     // algorithms: ['RS256'],
-  //     // audience: '<Your audience (aud) GUID>',
-  //     // issuer: '<Your issuer (iss) value>'
-  //   };
-
-  //   // Verify
-  //   var verifiedToken = jwt.verify(token, publicKey) //, verifyOptions);
-  //   console.log(verifiedToken); ˇ
-  // }
-
-  getRoles() {
-    let userRoles = [];
-    var encodedData = localStorage.getItem('msal.idtoken');
-    var blocks = encodedData.split('.');
-    var croppedData = blocks[1];
-    var bytes = base64.decode(croppedData);
-    var userInfo = utf8.decode(bytes);
-    var parsedInfo = JSON.parse(userInfo);
-    // ['testy', 'admin']
-    if (parsedInfo.roles != null) {
-      userRoles = parsedInfo.roles;
-    }
-    console.log("User roles is " + userRoles);
-    return userRoles;
   }
 
   render() {
     let adminComponent;
     let adminAddCourse;
-    var adminS = this.state.admin;
-    // this.getJwksUrl()
-    // this.getSigningKey()
-    // this.getIdToken()
+
     if (this.state.admin) {
       adminComponent = <h1>HELLO ADMIN</h1>;
       adminAddCourse = <Route path="/admin" component={Admin} />;
@@ -132,7 +56,7 @@ class App extends React.Component {
             <Navbar />
             <div>
               <Route exact path="/" component={Home} />
-              <Route path="/courses" render={() => <Courses adminStatus={adminS} />} />
+              <Route path="/courses" render={() => <Courses adminStatus={this.state.admin} />} />
               {adminAddCourse}
             </div>
             {adminComponent}
