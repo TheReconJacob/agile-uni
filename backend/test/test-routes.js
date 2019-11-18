c4ec2486932afef9ee434e701dd00f317972b9d9
skerry1 <sheila.kerry@sky.uk>
2019-11-18 11:32:47 +0000

    modified before statement in test-routes

--- backend/test/test-routes.js
+++ backend/test/test-routes.js
@@ -1,7 +1,6 @@
-const expect = require('chai').expect;
-const request = require('request');
-const fs = require('fs')
-
+const expect = require("chai").expect;
+const request = require("request");
+const fs = require("fs");
 
 //BEST PRACTICE - GET THIS WORKING but wer were having issues with the audience claim in the JWT token so we are manually passing a token
 // const AuthenticationContext = require('adal-node').AuthenticationContext;
@@ -26,85 +25,123 @@ const fs = require('fs')
 // console.log(tokenResponse)
 // console.log('outside')
 
+describe("Running app and testing data routes", function() {
+  //Used to have a timeout, now default before_all is set to 10000
+  before(done => {
+    server = require("../server.js");
+    server.on("app_started", function() {
+      done();
+    });
+  });
 
-describe('Running app and testing data routes', function () {
+  describe("Dummy tests", function() {
+    it("Dummy test", function(done) {
+      request(
+        "http://localhost:5000",
+        {
+          auth: {
+            bearer: process.env.AUTHTOKEN
+          }
+        },
+        function(error, response, body) {
+          expect(body).to.equal("Hello World");
+          done();
+        }
+      );
+    });
 
-	//Used to have a timeout, now default before_all is set to 10000
-	beforeEach(done => {
-		server = require('../server.js');
-		server.on("app_started", function () {
-			done()
-		})
-	});
+    it('Columns of employee data are correct', function (done) {
+    	request('http://localhost:5000/employees', {
+    		auth: {
+    			'bearer': process.env.AUTHTOKEN
+    		}
+    	}, function (error, response, body) {
+    		const rows = JSON.parse(body)['employees']['responseJson'];
+    		expect(rows[0]).to.have.all.keys('id', 'name', 'object_id', 'email');
+    		done();
+    	});
+    });
 
-	describe('Dummy tests', function () {
+    it("Columns of courses data are correct", function(done) {
+      request(
+        "http://localhost:5000/courses",
+        {
+          auth: {
+            bearer: process.env.AUTHTOKEN
+          }
+        },
+        function(error, response, body) {
+          const rows = JSON.parse(body)["courses"]["responseJson"];
+          expect(rows[0]).to.have.all.keys(
+            "course_id",
+            "title",
+            "description",
+            "start_date",
+            "end_date",
+            "attendees_max",
+            "attendees_booked",
+            "location",
+            "site_id",
+            "instructor_id"
+          );
+          done();
+        }
+      );
+    });
 
+    it("Columns of sites data are correct", function(done) {
+      request(
+        "http://localhost:5000/sites",
+        {
+          auth: {
+            bearer: process.env.AUTHTOKEN
+          }
+        },
+        function(error, response, body) {
+          const rows = JSON.parse(body)["sites"]["responseJson"];
+          expect(rows[0]).to.have.all.keys("id", "name", "address");
+          done();
+        }
+      );
+    });
+  });
 
-		it('Dummy test', function (done) {
-			request('http://localhost:5000', {
-				auth: {
-					'bearer': process.env.AUTHTOKEN
-				}
-			}, function (error, response, body) {
-				expect(body).to.equal('Hello World');
-				done();
-			});
-		});
+  describe("Search functions", function() {
+    it("Location: Osterley, Search: agile, should return stuff", function(done) {
+      request(
+        "http://localhost:5000/search?searchTerm=agile&location=Osterley",
+        {
+          auth: {
+            bearer: process.env.AUTHTOKEN
+          }
+        },
+        function(error, response, body) {
+          expect(
+            JSON.parse(body)["courses"]["responseJson"][0]
+          ).to.have.all.keys(
+            "course_id",
+            "title",
+            "description",
+            "start_date",
+            "end_date",
+            "attendees_max",
+            "attendees_booked",
+            "location",
+            "site_id",
+            "instructor_id",
+            "id",
+            "address",
+            "name"
+          );
+          done();
+        }
+      );
+    });
+  });
 
-		// it('Columns of employee data are correct', function (done) {
-		// 	request('http://localhost:5000/employees', {
-		// 		auth: {
-		// 			'bearer': process.env.AUTHTOKEN
-		// 		}
-		// 	}, function (error, response, body) {
-		// 		const rows = JSON.parse(body)['employees']['responseJson'];
-		// 		expect(rows[0]).to.have.all.keys('id', 'name', 'object_id', 'email');
-		// 		done();
-		// 	});
-		// });
-
-		it('Columns of courses data are correct', function (done) {
-			request('http://localhost:5000/courses', {
-				auth: {
-					'bearer': process.env.AUTHTOKEN
-				}
-			}, function (error, response, body) {
-				const rows = JSON.parse(body)['courses']['responseJson'];
-				expect(rows[0]).to.have.all.keys('course_id', 'title', 'description', 'start_date', 'end_date', 'attendees_max', 'attendees_booked', 'location', 'site_id', 'instructor_id');
-				done();
-			});
-		});
-
-		it('Columns of sites data are correct', function (done) {
-			request('http://localhost:5000/sites', {
-				auth: {
-					'bearer': process.env.AUTHTOKEN
-				}
-			}, function (error, response, body) {
-				const rows = JSON.parse(body)['sites']['responseJson'];
-				expect(rows[0]).to.have.all.keys('id', 'name', 'address');
-				done();
-			});
-		});
-	});
-
-	describe('Search functions', function () {
-		it('Location: Osterley, Search: agile, should return stuff', function (done) {
-			request('http://localhost:5000/search?searchTerm=agile&location=Osterley', {
-				auth: {
-					'bearer': process.env.AUTHTOKEN
-				}
-			}, function (error, response, body) {
-				expect(JSON.parse(body)['courses']['responseJson'][0]).to.have.all.keys('course_id', 'title', 'description', 'start_date', 'end_date', 'attendees_max', 'attendees_booked', 'location', 'site_id', 'instructor_id', 'id', 'address', 'name');
-				done();
-			});
-		});
-	})
-
-
-	after(done => {
-		delete require.cache[require.resolve('../server.js')]
-		server.closeServer();
-		done()
-	});
+  after(done => {
+    delete require.cache[require.resolve("../server.js")];
+    server.closeServer();
+    done();
+  });
 });
