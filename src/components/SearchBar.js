778f38ec6101d6a22c83002a5b37e82294ee769b
Emily Hall <emily.hall@sky.uk>
2019-11-19 11:35:07 +0000

    Authorized get request, search results now print in the console

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import axios from "axios";
 import "../styles/searchBar.scss";
 
+axios.defaults.headers.common["Authorization"] =
+  "Bearer " + localStorage.getItem("msal.idtoken");
+
 function getSearch(searchObj) {
   axios
     .get("http://localhost:5000/search", {
@@ -25,15 +28,21 @@ class SearchBar extends Component {
       searchTerm: ""
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(evt) {
-    this.setState({ [evt.target.name]: evt.target.value });
+    this.setState({ searchTerm: evt.target.value });
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    getSearch(this.state.searchTerm);
+    
   }
 
   render() {
     return (
       <div className="o-container">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <fieldset>
             <ul className="c-form-list o-layout--spaced">
               <li className="c-form-list">
@@ -60,13 +69,15 @@ class SearchBar extends Component {
                       className="c-form-combo__input c-form-input"
                       placeholder="Search for your next course"
                       id="f-combo"
+                      value={this.state.value}
                       onChange={this.handleChange}
                     />
                   </div>
                   <div className="c-form-combo__cell">
                     <button
                       className="c-form-combo__btn c-btn c-btn--primary"
-                      onClick={getSearch(this.searchTerm)}
+                      type="submit"
+                      value="Submit"
                     >
                       Search
                     </button>
