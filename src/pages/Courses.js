255b57a7eb21216929a3f9a6c8bdbc0b6c433966
Emily Hall <emily.hall@sky.uk>
2019-11-19 14:19:48 +0000

    Displaying dummy data in accordian components

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Accordion } from '@sky-uk/toolkit-react';
-import { AccordionSection } from '@sky-uk/toolkit-react';
+import { Accordion } from "@sky-uk/toolkit-react";
+import { AccordionSection } from "@sky-uk/toolkit-react";
 import SearchBar from "../components/SearchBar";
 import "../styles/courses.scss";
+import searchResponse from "./exampleJson.json";
 
 class Courses extends React.Component {
   constructor() {
@@ -18,60 +19,94 @@ class Courses extends React.Component {
   };
 
   render() {
-
     const parentId = "1";
     let adminAddComponent;
     let adminDeleteComponent;
     let adminEditComponent;
     if (this.props.adminStatus) {
-      adminAddComponent = <a href="/admin">
-                            <button className="c-btn c-btn--primary c-btn--full u-margin-bottom">
-                                <img className="c-btn__icon" src="https://www.sky.com/assets/toolkit/docs/buttons/example.svg" alt="Example Icon" />
-                                Add a new course
-                            </button>
-                          </a>;
-      adminDeleteComponent = <a href="courses" className="accordion-button c-btn c-btn--primary u-margin-right">Delete</a>;
-      adminEditComponent = <a href="/admin" className="accordion-button c-btn c-btn--primary u-margin-right">Edit</a>;
+      adminAddComponent = (
+        <a href="/admin">
+          <button className="c-btn c-btn--primary c-btn--full u-margin-bottom">
+            <img
+              className="c-btn__icon"
+              src="https://www.sky.com/assets/toolkit/docs/buttons/example.svg"
+              alt="Example Icon"
+            />
+            Add a new course
+          </button>
+        </a>
+      );
+      adminDeleteComponent = (
+        <a
+          href="courses"
+          className="accordion-button c-btn c-btn--primary u-margin-right"
+        >
+          Delete
+        </a>
+      );
+      adminEditComponent = (
+        <a
+          href="/admin"
+          className="accordion-button c-btn c-btn--primary u-margin-right"
+        >
+          Edit
+        </a>
+      );
     }
 
     return (
       <>
-      <div className="c-hero hero-background">
-        <div className="hero-title">
-          <p className="hero-title-text">Find your next course...</p>
-          <SearchBar />
+        <div className="c-hero hero-background">
+          <div className="hero-title">
+            <p className="hero-title-text">Find your next course...</p>
+            <SearchBar />
+          </div>
         </div>
-      </div>
 
         <div className="o-container course-page-accordion">
-          <div className="o-layout">
-            {adminAddComponent}
-            <Accordion
-              id={parentId}
-              collapseOnToggle
-              selected={this.state.accordionSelected}
-              updateSelection={this.updateAccordionSelection}
-              isNested="true"
-            >
-              <AccordionSection className="accordion-section" id="1" title="Section 1">
-                <div className="">
-                  <h2 className="c-heading-delta o-layout__item">
-                    Title
-                  </h2>
-                  <p className="c-text-body o-layout__item">
-                    Description
-                  </p>
-                  <div className="accordion-button-box">
-                    <a href="mailto:agileuniversity@sky.uk" className="accordion-button c-btn c-btn--primary u-margin-right">Request more information</a>
-                    <a href="/courses" className="accordion-button c-btn c-btn--primary u-margin-right">Book now</a>
-                    {adminDeleteComponent}
-                    {adminEditComponent}
+          <div className="o-layout">{adminAddComponent}</div>
+          <Accordion
+            id={parentId}
+            collapseOnToggle
+            selected={this.state.accordionSelected}
+            updateSelection={this.updateAccordionSelection}
+            isNested="true"
+          >
+            {searchResponse.map(function(res) {
+              return (
+                <AccordionSection
+                  className="accordion-section"
+                  id={res.course_id}
+                  title={res.title}
+                >
+                  <div className="">
+                    <h2 className="c-heading-delta o-layout__item">
+                      {res.title}
+                    </h2>
+                    <p className="c-text-body o-layout__item">
+                      {res.description}
+                    </p>
+                    <div className="accordion-button-box">
+                      <a
+                        href="mailto:agileuniversity@sky.uk"
+                        className="accordion-button c-btn c-btn--primary u-margin-right"
+                      >
+                        Request more information
+                      </a>
+                      <a
+                        href="/courses"
+                        className="accordion-button c-btn c-btn--primary u-margin-right"
+                      >
+                        Book now
+                      </a>
+                      {adminDeleteComponent}
+                      {adminEditComponent}
+                    </div>
                   </div>
-                </div>
-              </AccordionSection>
-              <AccordionSection className="accordion-section" id="2" title="Section 2"/>
-            </Accordion>
-          </div>
+                </AccordionSection>
+              );
+            })}
+          </Accordion>
         </div>
       </>
     );
