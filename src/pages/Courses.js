c0a0005115f85a4734acffeba35d11feb84bcb8b
skerry1 <sheila.kerry@sky.uk>
2019-11-20 11:31:00 +0000

    added warning for delete button

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Accordion } from '@sky-uk/toolkit-react';
-import { AccordionSection } from '@sky-uk/toolkit-react';
+import { Accordion } from "@sky-uk/toolkit-react";
+import { AccordionSection } from "@sky-uk/toolkit-react";
 import SearchBar from "../components/SearchBar";
 import "../styles/courses.scss";
 import DeleteButton from "../components/DeleteButton";
@@ -19,30 +19,49 @@ class Courses extends React.Component {
   };
 
   render() {
-
     const parentId = "1";
     let adminAddComponent;
     let adminDeleteComponent;
     let adminEditComponent;
     if (this.props.adminStatus) {
-      adminAddComponent = <a href="/admin">
-                            <button className="c-btn c-btn--primary c-btn--full u-margin-bottom">
-                                <img className="c-btn__icon" src="https://www.sky.com/assets/toolkit/docs/buttons/example.svg" alt="Example Icon" />
-                                Add a new course
-                            </button>
-                          </a>;
-      adminDeleteComponent = <a href="courses" className="accordion-button c-btn c-btn--primary u-margin-right">Delete</a>;
-      adminEditComponent = <a href="/admin" className="accordion-button c-btn c-btn--primary u-margin-right">Edit</a>;
+      adminAddComponent = (
+        <a href="/admin">
+          <button className="c-btn c-btn--primary c-btn--full u-margin-bottom">
+            <img
+              className="c-btn__icon"
+              src="https://www.sky.com/assets/toolkit/docs/buttons/example.svg"
+              alt="Example Icon"
+            />
+            Add a new course
+          </button>
+        </a>
+      );
+      // adminDeleteComponent = (
+      //   <a
+      //     href="courses"
+      //     className="accordion-button c-btn c-btn--primary u-margin-right"
+      //   >
+      //     Delete
+      //   </a>
+      // );
+      adminEditComponent = (
+        <a
+          href="/admin"
+          className="accordion-button c-btn c-btn--primary u-margin-right"
+        >
+          Edit
+        </a>
+      );
     }
 
     return (
       <>
-      <div className="c-hero hero-background">
-        <div className="hero-title">
-          <p className="hero-title-text">Find your next course...</p>
-          <SearchBar />
+        <div className="c-hero hero-background">
+          <div className="hero-title">
+            <p className="hero-title-text">Find your next course...</p>
+            <SearchBar />
+          </div>
         </div>
-      </div>
 
         <div className="o-container course-page-accordion">
           <div className="o-layout">
@@ -54,24 +73,40 @@ class Courses extends React.Component {
               updateSelection={this.updateAccordionSelection}
               isNested="true"
             >
-              <AccordionSection className="accordion-section" id="1" title="Section 1">
+              <AccordionSection
+                className="accordion-section"
+                id="1"
+                title="Section 1"
+              >
                 <div className="">
-                  <h2 className="c-heading-delta o-layout__item">
-                    Title
-                  </h2>
-                  <p className="c-text-body o-layout__item">
-                    Description
-                  </p>
+                  <h2 className="c-heading-delta o-layout__item">Title</h2>
+                  <p className="c-text-body o-layout__item">Description</p>
                   <div className="accordion-button-box">
-                    <a href="mailto:agileuniversity@sky.uk" className="accordion-button c-btn c-btn--primary u-margin-right">Request more information</a>
-                    <a href="/courses" className="accordion-button c-btn c-btn--primary u-margin-right">Book now</a>
-                    <DeleteButton adminStatus={this.props.adminStatus} courseToDelete={AccordionSection.id}/>
-                    {/* {adminDeleteComponent} */}
+                    <a
+                      href="mailto:agileuniversity@sky.uk"
+                      className="accordion-button c-btn c-btn--primary u-margin-right"
+                    >
+                      Request more information
+                    </a>
+                    <a
+                      href="/courses"
+                      className="accordion-button c-btn c-btn--primary u-margin-right"
+                    >
+                      Book now
+                    </a>
+                    <DeleteButton
+                      adminStatus={this.props.adminStatus}
+                      courseToDelete={AccordionSection.id}
+                    />
                     {adminEditComponent}
                   </div>
                 </div>
               </AccordionSection>
-              <AccordionSection className="accordion-section" id="2" title="Section 2"/>
+              <AccordionSection
+                className="accordion-section"
+                id="2"
+                title="Section 2"
+              />
             </Accordion>
           </div>
         </div>
