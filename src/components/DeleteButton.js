d5a120ace8aefb348e0805450f781f079f3c8649
skerry1 <sheila.kerry@sky.uk>
2019-11-20 11:31:00 +0000

    added warning for delete button

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,25 +1,37 @@
 import React from "react";
 
-
 class DeleteButton extends React.Component {
-    constructor() {
-      super();
-    }
-  
-    render() {
-  
-      let adminDeleteComponent;
-      console.log("id to delete"+this.props.courseToDelete);
-      if (this.props.adminStatus) {
-        adminDeleteComponent = <a href="courses" className="accordion-button c-btn c-btn--primary u-margin-right">Delete</a>;
-      }
-  
-      return (
-        <>
-        {adminDeleteComponent}
-        </>
-      );
+  constructor() {
+    super();
+  }
+
+  confirmDelete() {
+
+    
+    var answer = window.confirm("Save data?");
+    if (answer) {
+      //some code
+    } else {
+      //some code
     }
   }
-  
-  export default DeleteButton;
+
+  render() {
+    let adminDeleteComponent;
+    console.log("id to delete" + this.props.courseToDelete);
+    if (this.props.adminStatus) {
+      adminDeleteComponent = (
+        <button
+          onClick={this.confirmDelete}
+          className="accordion-button c-btn c-btn--primary u-margin-right"
+        >
+          Delete
+        </button>
+      );
+    }
+
+    return <>{adminDeleteComponent}</>;
+  }
+}
+
+export default DeleteButton;
