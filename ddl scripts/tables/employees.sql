cf3864022c98fc634e687d1e84446b5cc4838ee1
DESKTOP-5FL1D5O\Nasir <n>
2019-11-25 14:58:19 +0000

    refactored schema

--- ddl scripts/tables/employees.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `employees` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `object_id` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1066 DEFAULT CHARSET=latin1;
