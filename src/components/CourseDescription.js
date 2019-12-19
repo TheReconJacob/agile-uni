e769a8ff64502c153fdaee5784946e56373de78d
mini-ninja-64 <callumhart@btinternet.com>
2019-12-19 13:39:57 +0000

    Refactor CourseDescription to be functional

--- src/components/CourseDescription.js
+++ src/components/CourseDescription.js
@@ -1,26 +1,24 @@
 import React from "react";
 
-class CourseDescription extends React.Component {
-  render() {
-    let divText = this.props.courseId + "description";
-    let childDivText = this.props.courseId + "child";
-    let htmlString =
-      "<div id=" + childDivText + ">" + this.props.CourseDescription + "</div>";
-    try {
-      let parentDiv = document.getElementById(divText);
-      let childDiv = document.getElementById(childDivText);
-      let range = document.createRange();
-      let documentFragment = range.createContextualFragment(htmlString);
-      parentDiv.replaceChild(documentFragment, childDiv);
-    } catch {}
-    return (
-      <p className="c-text-body o-layout__item">
-        <div id={divText}>
-          <div id={childDivText} />
-        </div>
-      </p>
-    );
-  }
+function CourseDescription(props) {
+  let divText = props.courseId + "description";
+  let childDivText = props.courseId + "child";
+  let htmlString =
+    "<div id=" + childDivText + ">" + props.CourseDescription + "</div>";
+  try {
+    let parentDiv = document.getElementById(divText);
+    let childDiv = document.getElementById(childDivText);
+    let range = document.createRange();
+    let documentFragment = range.createContextualFragment(htmlString);
+    parentDiv.replaceChild(documentFragment, childDiv);
+  } catch {}
+  return (
+    <p className="c-text-body o-layout__item">
+      <div id={divText}>
+        <div id={childDivText} />
+      </div>
+    </p>
+  );
 }
 
 export default CourseDescription;
