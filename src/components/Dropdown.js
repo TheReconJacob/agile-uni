11f2401baeab6a8ddd0e36b2f9f258619555060f
mini-ninja-64 <callumhart@btinternet.com>
2019-12-19 10:56:42 +0000

    Refactor Dropdown to be functional

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,37 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import "../styles/dropdown.scss";
 
-class DropdownSite extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.componentDidUpdate = this.componentDidUpdate.bind(this);
-  }
-
-  componentDidUpdate(prevProps) {}
-
-  handleChange(evt) {
-    this.props.setSite(parseInt(evt.target.value, 10));
-  }
-
-  render() {
-    let options = this.props.state.options;
-    let optionItems = options.map(options => (
-      <option key={options.id} value={options.id}>
-        {options.name}
-      </option>
-    ));
-    return (
-      <select
-        className="c-form-select__dropdown--inline o-layout__item u-width-1/4"
-        style={{ height: 39 }}
-        onChange={this.handleChange}
-      >
-        <option value="">Select a location</option>
-        {optionItems}
-      </select>
-    );
-  }
+function DropdownSite(props) {
+  let options = props.state.options;
+  console.log(props);
+  console.log(options);
+  let optionItems = options.map(options => (
+    <option key={options.id} value={options.id}>
+      {options.name}
+    </option>
+  ));
+  return (
+    <select
+      className="c-form-select__dropdown--inline o-layout__item u-width-1/4"
+      style={{ height: 39 }}
+      onChange={event => props.setSite(parseInt(event.target.value, 10))}
+    >
+      <option value="">Select a location</option>
+      {optionItems}
+    </select>
+  );
 }
 
 export default DropdownSite;
