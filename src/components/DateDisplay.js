d188fcc14569c0ccad381e73b0b3a9ada96a2e19
mini-ninja-64 <callumhart@btinternet.com>
2019-12-19 12:02:26 +0000

    Refactor DateDisplay component to be functional

--- src/components/DateDisplay.js
+++ src/components/DateDisplay.js
@@ -1,39 +1,37 @@
 import React from "react";
 
-class DateDisplay extends React.Component {
-  render() {
-    let formattedStart = "";
-    let formattedEnd = "";
-    try {
-      formattedStart =
-        new Intl.DateTimeFormat("en-GB", {
-          year: "numeric",
-          month: "long",
-          day: "2-digit"
-        }).format(Date.parse(this.props.start_date)) +
-        " at " +
-        this.props.start_date.slice(11, 16);
-      formattedEnd =
-        new Intl.DateTimeFormat("en-GB", {
-          year: "numeric",
-          month: "long",
-          day: "2-digit"
-        }).format(Date.parse(this.props.end_date)) +
-        " at " +
-        this.props.end_date.slice(11, 16);
-    } catch {}
-    let dateElement = (
-      <>
-        <p className="c-text-body o-layout__item">
-          <b>Start: {formattedStart}</b>
-        </p>
-        <p className="c-text-body o-layout__item">
-          <b>End: {formattedEnd}</b>
-        </p>
-      </>
-    );
-    return <>{dateElement}</>;
+function dateTimeFormatter(dateString) {
+  let formattedDateTime = new Intl.DateTimeFormat("en-GB", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
+  }).format(Date.parse(dateString));
+  return formattedDateTime;
+}
+
+function DateDisplay(props) {
+  let formattedStart = "";
+  let formattedEnd = "";
+  try {
+    formattedStart = dateTimeFormatter(props.start_date);
+    formattedEnd = dateTimeFormatter(props.end_date);
+  } catch (e) {
+    console.log("Error", e.stack);
   }
+
+  return (
+    <>
+      <p className="c-text-body o-layout__item">
+        <b>Start: {formattedStart}</b>
+      </p>
+      <p className="c-text-body o-layout__item">
+        <b>End: {formattedEnd}</b>
+      </p>
+    </>
+  );
 }
 
 export default DateDisplay;
