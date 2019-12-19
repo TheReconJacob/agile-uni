ef950344f15ec919484af50d27064fbd5dfac882
joebelton <joebelton@live.co.uk>
2019-12-19 16:50:59 +0000

    Add reusable modal and button components
    As part of refactoring created reusable components, ease of implemention

---
+++ src/components/reusable/Modal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Modal } from "@sky-uk/toolkit-react";
+import ButtonComponent from "./Button";
+import ModalData from "./reusableData/ModalData";
+
+function ModalComponent(props) {
+  const { isOpen, onCloseFunction, textToDisplay, modalButtons } = props;
+
+  return (
+    <Modal isOpen={isOpen} onClose={onCloseFunction}>
+      <p>{ModalData[textToDisplay].text}</p>
+      {modalButtons.map(([text, onClick], index) => {
+        return <ButtonComponent key={index} text={text} onClick={onClick} />;
+      })}
+    </Modal>
+  );
+}
+
+export default ModalComponent;
