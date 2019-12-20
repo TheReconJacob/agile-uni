95a780847d98b4c550f435f5408da13f4d31e7e8
jack272hughes <jack272hughes@gmail.com>
2019-12-20 10:58:41 +0000

    Make small changes to existing functions
    The find() function now returns a single value instead of array
    Renamed close to endConnection
    Updated syntax

--- backend/data/dataAccessor.js
+++ backend/data/dataAccessor.js
@@ -3,19 +3,24 @@ const mysql = require("mysql"),
   connection = mysql.createConnection(config),
   dataAccessor = {};
 
-function sendQueryAndReturnResultsAsPromise(query, inputs) {
+function sendQueryAndReturnResultsAsPromise(
+  query,
+  inputs,
+  shouldReturnSingleValue
+) {
   return new Promise((resolve, reject) => {
     connection.query(query, inputs, (error, data) => {
       if (error) {
         reject({ error, data: false, status: 500 });
       } else {
+        if (shouldReturnSingleValue) data = data[0];
         resolve({ error: false, data, status: 200 });
       }
     });
   });
 }
 
-dataAccessor.close = () => {
+dataAccessor.endConnection = () => {
   connection.end();
 };
 
@@ -36,7 +41,7 @@ dataAccessor.courses = {
   find: ({ course_id, courseTitleFragment, site_id }) => {
     if (course_id) {
       const query = "SELECT * FROM courses WHERE course_id = ?";
-      return sendQueryAndReturnResultsAsPromise(query, [course_id]);
+      return sendQueryAndReturnResultsAsPromise(query, [course_id], true);
     }
 
     const inputs = [];
@@ -77,7 +82,6 @@ dataAccessor.courses = {
   update: newValuesObject => {
     const { course_id } = newValuesObject;
     delete newValuesObject.course_id;
-    let query = "UPDATE courses SET ";
     const filter = [],
       inputs = [];
 
@@ -87,11 +91,11 @@ dataAccessor.courses = {
     });
 
     inputs.push(course_id);
-    query += filter.join(", ") + " WHERE course_id = ?";
+    const query = `UPDATE courses SET ${filter.join(", ")} WHERE course_id = ?`;
     return sendQueryAndReturnResultsAsPromise(query, inputs);
   },
 
-  delete: ({ course_id }) => {
+  delete: course_id  => {
     let query = "DELETE FROM courses";
     if (course_id === "all") return sendQueryAndReturnResultsAsPromise(query);
 
