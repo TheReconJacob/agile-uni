93cd053379d2c25d06600d1f076415d02952825d
jack272hughes <jack272hughes@gmail.com>
2020-01-09 16:01:34 +0000

    Add courses seed data for testing

---
+++ ddl_scripts/testdb_0.sql
@@ -0,0 +1,6 @@
+INSERT INTO courses
+    (title, description, start_date, end_date, attendees_max, location, site_id, instructor_name)
+VALUES
+    ("Course 1", "<p>Description 1</p>", "2020-09-23 09:00:00", "2020-09-23 17:00:00", 12, "Room 1.1", 1, "Barry Allen"),
+    ("Course 2", "<p>Description 2</p>", "2020-05-18 10:00:00", "2020-05-18 12:00:00", 12, "Room 2.3", 1, "Bruce Wayne"),
+    ("Course 3", "<p>Description 3</p>", "2020-07-12 12:00:00", "2020-07-12 13:00:00", 12, "Room 3.7", 1, "Wayde Wilson");
