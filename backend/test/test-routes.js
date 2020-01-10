077332866b623084e8b887c35fdea011f3c81624
jack272hughes <jack272hughes@gmail.com>
2020-01-10 11:30:08 +0000

    Rename clientSecret and make it a json
    Preperation for giving the tests a way to get a jwt token

--- backend/test/test-routes.js
+++ backend/test/test-routes.js
@@ -17,7 +17,7 @@ let server;
 // const AuthenticationContext = require('adal-node').AuthenticationContext;
 // const authorityUrl = 'https://login.microsoftonline.com/68b865d5-cf18-4b2b-82a4-a4eddb9c5237';
 // const applicationId = 'c0fb79ba-b72c-47c1-912c-48ee6cbac972'
-// const clientSecret = fs.readFileSync('../clientSecret', 'UTF-8')
+// const clientSecret = require("../../clientSecret.json")[token]
 // const resource = '00000003-0000-0000-c000-000000000000'
 // const context = new AuthenticationContext(authorityUrl, true, null, null);
 // let server;
@@ -36,17 +36,17 @@ let server;
 // console.log(tokenResponse)
 // console.log('outside')
 
-describe("Integration tests: Running app and testing data routes", function() {
+describe("Integration tests: Running app and testing data routes", () => {
   //Used to have a timeout, now default before_all is set to 10000
   before(done => {
     server = require("../server.js");
-    server.on("app_started", function() {
+    server.on("app_started", () => {
       done();
     });
   });
 
-  describe("Dummy tests", function() {
-    it("Dummy test", function(done) {
+  describe("Dummy tests", () => {
+    it("Dummy test", done => {
       request(
         "http://localhost:5000",
         {
@@ -54,14 +54,14 @@ describe("Integration tests: Running app and testing data routes", function() {
             bearer: process.env.AUTHTOKEN
           }
         },
-        function(error, response, body) {
+        (error, response, body) => {
           expect(body).to.equal("Hello World");
           done();
         }
       );
     });
 
-    it("Columns of employee data are correct", function(done) {
+    it("Columns of employee data are correct", done => {
       request(
         "http://localhost:5000/employees",
         {
@@ -69,7 +69,7 @@ describe("Integration tests: Running app and testing data routes", function() {
             bearer: process.env.AUTHTOKEN
           }
         },
-        function(error, response, body) {
+        (error, response, body) => {
           const rows = JSON.parse(body)["employees"]["responseJson"];
           expect(rows[0]).to.have.all.keys("id", "name", "object_id", "email");
           done();
@@ -77,7 +77,7 @@ describe("Integration tests: Running app and testing data routes", function() {
       );
     });
 
-    it("Columns of courses data are correct", function(done) {
+    it("Columns of courses data are correct", done => {
       request(
         "http://localhost:5000/courses",
         {
@@ -85,7 +85,7 @@ describe("Integration tests: Running app and testing data routes", function() {
             bearer: process.env.AUTHTOKEN
           }
         },
-        function(error, response, body) {
+        (error, response, body) => {
           const rows = JSON.parse(body)["courses"]["responseJson"];
           expect(rows[0]).to.have.all.keys(
             "course_id",
@@ -104,7 +104,7 @@ describe("Integration tests: Running app and testing data routes", function() {
       );
     });
 
-    it("Columns of sites data are correct", function(done) {
+    it("Columns of sites data are correct", done => {
       request(
         "http://localhost:5000/sites",
         {
@@ -112,7 +112,7 @@ describe("Integration tests: Running app and testing data routes", function() {
             bearer: process.env.AUTHTOKEN
           }
         },
-        function(error, response, body) {
+        (error, response, body) => {
           const rows = JSON.parse(body)["sites"]["responseJson"];
           expect(rows[0]).to.have.all.keys("id", "name", "address");
           done();
@@ -121,9 +121,9 @@ describe("Integration tests: Running app and testing data routes", function() {
     });
   });
 
-  describe("Display courses routes", function() {
-    describe("Search functions", function() {
-      it("Site: Osterley, Search: agile, should return stuff", function(done) {
+  describe("Display courses routes", () => {
+    describe("Search functions", () => {
+      it("Site: Osterley, Search: agile, should return stuff", done => {
         request(
           "http://localhost:5000/search?searchTerm=agile&site=Osterley",
           {
@@ -131,7 +131,7 @@ describe("Integration tests: Running app and testing data routes", function() {
               bearer: process.env.AUTHTOKEN
             }
           },
-          function(error, response, body) {
+          (error, response, body) => {
             expect(
               JSON.parse(body)["courses"]["responseJson"][0]
             ).to.have.all.keys(
@@ -155,8 +155,8 @@ describe("Integration tests: Running app and testing data routes", function() {
       });
     });
 
-    describe("List all courses", function() {
-      it("List all courses should match the specificed keys", function(done) {
+    describe("List all courses", () => {
+      it("List all courses should match the specificed keys", done => {
         request(
           "http://localhost:5000/listAllCourses",
           {
@@ -164,7 +164,7 @@ describe("Integration tests: Running app and testing data routes", function() {
               bearer: process.env.AUTHTOKEN
             }
           },
-          function(error, response, body) {
+          (error, response, body) => {
             const rows = JSON.parse(body)["courses"]["responseJson"];
             expect(rows[0]).to.have.all.keys(
               "course_id",
@@ -188,9 +188,9 @@ describe("Integration tests: Running app and testing data routes", function() {
     });
   });
 
-  describe("Course admin routes", function() {
-    describe("Add a course", function() {
-      it("Course should be added successfully", function(done) {
+  describe("Course admin routes", () => {
+    describe("Add a course", () => {
+      it("Course should be added successfully", done => {
         chai
           .request("http://localhost:5000")
           .post("/addCourse")
@@ -205,15 +205,15 @@ describe("Integration tests: Running app and testing data routes", function() {
             description: "Things",
             instructor_name: "Alex Drage"
           })
-          .end(function(error, response, body) {
+          .end((error, response, body) => {
             expect(response).to.have.status(200);
             done();
           });
       });
     });
 
-    describe("Update a course", function() {
-      it("Course should be edited successfully", function(done) {
+    describe("Update a course", () => {
+      it("Course should be edited successfully", done => {
         chai
           .request("http://localhost:5000")
           .post("/editCourse")
@@ -236,8 +236,8 @@ describe("Integration tests: Running app and testing data routes", function() {
       });
     });
 
-    describe("Add an employee", function() {
-      it("Employee should be added successfully", function(done) {
+    describe("Add an employee", () => {
+      it("Employee should be added successfully", done => {
         chai
           .request("http://localhost:5000")
           .post("/addEmployee")
@@ -247,7 +247,7 @@ describe("Integration tests: Running app and testing data routes", function() {
             object_id: "adoasjdoa",
             email: "employee@sky.uk"
           })
-          .end(function(error, response, body) {
+          .end((error, response, body) => {
             deleteEmployeeTestFunction(
               response.body.employees.responseJson.insertId
             );
@@ -263,8 +263,8 @@ describe("Integration tests: Running app and testing data routes", function() {
     });
   });
 
-  describe("Add an attendee", function() {
-    it("Adding an attendee to course", function(done) {
+  describe("Add an attendee", () => {
+    it("Adding an attendee to course", done => {
       addCourseWithId();
       addEmployee();
       function sleep(time) {
@@ -278,7 +278,7 @@ describe("Integration tests: Running app and testing data routes", function() {
               bearer: process.env.AUTHTOKEN
             }
           },
-          function(error, response, body) {
+          (error, response, body) => {
             const rows = JSON.parse(response.body)["combinedResponse"][0][
               "course_attendees"
             ]["responseJson"];
@@ -307,8 +307,8 @@ describe("Integration tests: Running app and testing data routes", function() {
     deleteCourse();
   });
 
-  describe("Delete an attendee", function() {
-    it("Deleting an attendee from course", function(done) {
+  describe("Delete an attendee", () => {
+    it("Deleting an attendee from course", done => {
       addCourseWithId();
       addEmployee();
       addEmployeeToCourse();
@@ -323,7 +323,7 @@ describe("Integration tests: Running app and testing data routes", function() {
               bearer: process.env.AUTHTOKEN
             }
           },
-          function(error, response, body) {
+          (error, response, body) => {
             const rows = JSON.parse(response.body)["combinedResponse"][0][
               "course_attendees"
             ]["responseJson"];
@@ -352,8 +352,8 @@ describe("Integration tests: Running app and testing data routes", function() {
     deleteCourse();
   });
 
-  describe("Delete a course", function() {
-    it("Deleting a course should return right response from server", function(done) {
+  describe("Delete a course", () => {
+    it("Deleting a course should return right response from server", done => {
       addCourseWithId();
 
       function sleep(time) {
@@ -367,7 +367,7 @@ describe("Integration tests: Running app and testing data routes", function() {
               bearer: process.env.AUTHTOKEN
             }
           },
-          function(error, response, body) {
+          (error, response, body) => {
             const rows = JSON.parse(response.body)["courses"]["responseJson"];
             expect(rows[1]["affectedRows"]).to.equal(1);
             expect(rows[1]["changedRows"]).to.equal(0);
@@ -381,10 +381,9 @@ describe("Integration tests: Running app and testing data routes", function() {
     });
   });
 
-
-  describe("Find course by ID", function() {
-    describe("Search courses by ID", function() {
-      it("Course should be returned based on ID", function(done) {
+  describe("Find course by ID", () => {
+    describe("Search courses by ID", () => {
+      it("Course should be returned based on ID", done => {
         request(
           "http://localhost:5000/findCourseById?course_id=1",
           {
@@ -392,53 +391,24 @@ describe("Integration tests: Running app and testing data routes", function() {
               bearer: process.env.AUTHTOKEN
             }
           },
-          function(error, response, body) {
+          (error, response, body) => {
             expect(
               JSON.parse(body)["courses"]["responseJson"][0]
             ).to.have.all.keys(
-				"course_id",
-				"title",
-				"description",
-				"start_date",
-				"end_date",
-				"attendees_max",
-				"attendees_booked",
-				"location",
-				"site_id",
-				"instructor_name",
-				"id",
-				"name",
-				"address"
+              "course_id",
+              "title",
+              "description",
+              "start_date",
+              "end_date",
+              "attendees_max",
+              "attendees_booked",
+              "location",
+              "site_id",
+              "instructor_name",
+              "id",
+              "name",
+              "address"
             );
-            done();
-          }
-        );
-      });
-	});
-});
-
-describe("Display if employee is booked on a course", function() {
-  describe("Display if booked", function() {
-    it("Should return a boolean value of 0 or 1", function(done) {
-      addCourseWithId();
-      addEmployee();
-      addEmployeeToCourse();
-      function sleep(time) {
-        return new Promise(resolve => setTimeout(resolve, time));
-      }
-      sleep(10000).then(() => {
-        request(
-          "http://localhost:5000/returnIfBooked?employee_id=999&course_id=2",
-          {
-            auth: {
-              bearer: process.env.AUTHTOKEN
-            }
-          },
-          function(error, response, body) {
-            console.log(body);
-            expect(
-              JSON.parse(body)["course_attendees"]["responseJson"][0]["EXISTS(SELECT * FROM AGILEUNI.course_attendees WHERE employee_id = '999' AND course_id = '2')"]
-            ).to.equal(1);
             done();
           }
         );
@@ -446,14 +416,45 @@ describe("Display if employee is booked on a course", function() {
     });
   });
 
-  deleteCourse();
-  function sleep(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-  }
-  sleep(15000).then(() => {
-    deleteEmployeeTestFunction("999");
+  describe("Display if employee is booked on a course", () => {
+    describe("Display if booked", () => {
+      it("Should return a boolean value of 0 or 1", done => {
+        addCourseWithId();
+        addEmployee();
+        addEmployeeToCourse();
+        function sleep(time) {
+          return new Promise(resolve => setTimeout(resolve, time));
+        }
+        sleep(10000).then(() => {
+          request(
+            "http://localhost:5000/returnIfBooked?employee_id=999&course_id=2",
+            {
+              auth: {
+                bearer: process.env.AUTHTOKEN
+              }
+            },
+            (error, response, body) => {
+              console.log(body);
+              expect(
+                JSON.parse(body)["course_attendees"]["responseJson"][0][
+                  "EXISTS(SELECT * FROM AGILEUNI.course_attendees WHERE employee_id = '999' AND course_id = '2')"
+                ]
+              ).to.equal(1);
+              done();
+            }
+          );
+        });
+      });
+    });
+
+    deleteCourse();
+    function sleep(time) {
+      return new Promise(resolve => setTimeout(resolve, time));
+    }
+    sleep(15000).then(() => {
+      deleteEmployeeTestFunction("999");
+    });
   });
-});
 
   after(done => {
     delete require.cache[require.resolve("../server.js")];
@@ -461,5 +462,3 @@ describe("Display if employee is booked on a course", function() {
     done();
   });
 });
-
-
