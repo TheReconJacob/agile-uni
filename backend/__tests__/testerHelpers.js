baf948556d93f626aaabb52f7385f800141cd306
jack272hughes <jack272hughes@gmail.com>
2020-01-13 16:13:10 +0000

    Update fake courses and add test function
    Each course now has more values for the different columns
    Added another function to add an attendee for a course

--- backend/__tests__/testerHelpers.js
+++ backend/__tests__/testerHelpers.js
@@ -1,19 +1,23 @@
 const config = require("../config"),
   mysql = require("mysql"),
   connection = mysql.createConnection(config),
-  addCourseQuery = `;INSERT INTO courses (id, title, description, start_date, site_id, location) VALUES (?, ?, ?, ?, ?, ?)`,
+  addCourseQuery = `;INSERT INTO courses (id, title, attendees_max, description, start_date, end_date, site_id, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
   inputs = [];
 let query = "DELETE FROM courses";
 
 function createInput({ title = "TEST", site_id = 1, course_id }) {
-  const start_date = new Date();
+  const start_date = "2020-06-23 09:00";
+  const end_date = "2020-06-23 17:00";
+  const attendees_max = 10;
 
   query += addCourseQuery;
   inputs.push(
     course_id,
     title,
+    attendees_max,
     "THIS IS A TEST",
     start_date,
+    end_date,
     site_id,
     "BUILDING"
   );
@@ -55,6 +59,15 @@ module.exports = {
         if (error) console.log(error);
         resolve(data);
       });
+    });
+  },
+
+  addAttendee: ({ course_id = 1, azure_oid = 1 }) => {
+    return new Promise(resolve => {
+      connection.query(
+        "INSERT INTO course_attendees (course_id, azure_oid, attended) VALUES (?, ?, ?)",
+        [course_id, azure_oid, false]
+      );
     });
   }
 };
