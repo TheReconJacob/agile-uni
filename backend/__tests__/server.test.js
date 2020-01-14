26a07d6401ab9177cd3ab2dbba19b7b3c8ffde9d
jack272hughes <jack272hughes@gmail.com>
2020-01-14 09:38:29 +0000

    Add tests for server
    Tests every route along with every possible query combination

--- backend/__tests__/server.test.js
+++ backend/__tests__/server.test.js
@@ -12,7 +12,6 @@ beforeAll(async () => {
 });
 
 afterAll(() => {
-  // closeServer();
   testerHelpers.endConnection();
 });
 
@@ -29,23 +28,6 @@ function postRequest(url, queryObject = {}) {
     .set("Authorization", `bearer ${process.env.AUTHTOKEN}`)
     .send(queryObject);
 }
-
-// function closeServer() {
-//   request.get("/").end();
-// }
-
-// - get / (on its ones)
-// - get /sites
-// - get /search and /search with parameters of courseTitleFragment and site_id where the fragment is a part of the course name and the id is 1, 2 or 3
-// - post /addCourse with parameters of title, description, start_date, end_date, attendees_max, location, site_id and instructor_name
-// - post /editCourse with same params as above
-// - get /deleteCourse with param of course_id
-// - get /addAttendee with params course_id and azure_oid (which is just an employee id from microsoft azure so make it a random number)
-// - get /deleteAttendee with params course_id and attendee_id
-// - get /attendees with param course_id
-// - get /totalAttendees with param course_id
-// - get /returnIfBooked with params course_id and employee_id
-// - get /findCourseById with param course_id
 
 describe("When testing the server.js it", () => {
   it("Should connect successfully and be able to return a response", async () => {
@@ -103,47 +85,71 @@ describe("When testing the server.js it", () => {
     const insertedRow = await testerHelpers.findById(response.body.insertId);
     courseObject.course_id = response.body.insertId;
 
-    expect(insertedRow).toBeTruthy();
+    expect(insertedRow.title).toBe("testingAddCourse");
   });
 
   it("Should update a course when posting to /editCourse", async () => {
     courseObject.title = "testingEditCourse";
     courseObject.description = "Changed Description";
-    const response = await postRequest("/editCourse", courseObject);
+    await postRequest("/editCourse", courseObject);
     const insertedRow = await testerHelpers.findById(courseObject.course_id);
+
+    expect(insertedRow.title).toBe("testingEditCourse");
+    expect(insertedRow.description).toBe("Changed Description");
   });
 
   it("Should delete a course when getting from /deleteCourse with param course_id", async () => {
-    const response = await getRequest("", {});
+    const response = await getRequest("/deleteCourse", {
+      courseId: courseObject.course_id
+    });
+
+    expect(response.body.affectedRows).toBe(1);
   });
 
   it("Should add an attendee when getting from /addAttendee with params course_id and azure_oid", async () => {
-    const response = await getRequest("", {});
+    const response = await getRequest("/addAttendee", {
+      course_id: 2,
+      azure_oid: 100
+    });
+    expect(response.body.affectedRows).toBe(1);
   });
-
   it("Should delete an attendee when getting from /deleteAttendee with params course_id and azure_oid", async () => {
-    const response = await getRequest("", {});
+    const response = await getRequest("/deleteAttendee", {
+      course_id: 2,
+      azure_oid: 100
+    });
+    expect(response.body.affectedRows).toBe(1);
   });
-
   it("Should get all attendees for course when getting from /attendees with param course_id", async () => {
-    const response = await getRequest("", {});
+    await testerHelpers.addAttendee({});
+    const response = await getRequest("/attendees", { course_id: 1 });
+    expect(response.body).toHaveLength(1);
   });
-
   it("Should get number of attendees for course when getting from /totalAttendees with param course_id", async () => {
-    const response = await getRequest("", {});
+    const response = await getRequest("/totalAttendees", { course_id: 1 });
+    expect(response.body).toBe(1);
   });
 
   describe("When getting from /returnIfBooked with params azure_oid and course_id it", () => {
     it("Should return false if the attendee is not booked", async () => {
-      const response = await getRequest("", {});
-    });
+      const response = await getRequest("/returnIfBooked", {
+        azure_oid: 999999,
+        course_id: 1
+      });
 
+      expect(response.body).toBe(false);
+    });
     it("Should return true if the attendee is booked", async () => {
-      const response = await getRequest("", {});
+      const response = await getRequest("/returnIfBooked", {
+        azure_oid: 1,
+        course_id: 1
+      });
+
+      expect(response.body).toBe(true);
     });
   });
-
   it("Should get the correct course when getting from /findCourseById with para course_id", async () => {
-    const response = await getRequest("/findCourseById", {});
+    const response = await getRequest("/findCourseById", { course_id: 1 });
+    expect(response.body.title).toBe("TEST");
   });
 });
