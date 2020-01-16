b2b86d701cdc3aa19305ab59c8dfbc0835393f68
jack272hughes <jack272hughes@gmail.com>
2020-01-16 15:51:27 +0000

    Refactor search bar
    Now uses FormBuilder to create the search bar components
    Uses react-router history component to handle search requests

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,80 +1,25 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+import { useHistory } from "react-router-dom";
 import "../styles/searchBar.scss";
-import { Dropdown } from "@sky-uk/toolkit-react";
-import { Link } from "react-router-dom";
+import FormBuilder from "./FormBuilder/FormBuilder";
+const FormJSON = require("../forms/SearchBarForm.json");
 
 function SearchBar() {
-  const [sites, setSites] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [siteId, setSiteId] = useState("");
-  const [popup, setPopup] = useState(false);
-  const [dropdownName, setDropdownName] = useState("Location");
+  const history = useHistory();
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/sites")
-      .then(response => {
-        const listItems = [{ text: "Location", value: "" }];
-        const sitesRaw = response.data;
+  const handleSubmit = event => {
+    const formObject = new FormData(event.target);
+    const siteId = formObject.get("search_siteId");
+    const searchTerm = formObject.get("search_courseTitleFragment");
 
-        sitesRaw.forEach(site =>
-          listItems.push({ text: site.name, value: site.id })
-        );
-
-        setSites(listItems);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
-
-  const handleSiteChange = event => {
-    setSiteId(event.value);
-    setDropdownName(event.text);
+    event.preventDefault();
+    history.location.pathname = "/courses";
+    history.push(`?searchTerm=${searchTerm}&siteId=${siteId}`);
   };
 
   return (
     <div className="o-container">
-      <form onSubmit={event => event.preventDefault()}>
-        <fieldset>
-          <ul className="c-form-list o-layout--spaced">
-            <li className="c-form-list">
-              <Dropdown
-                name={dropdownName}
-                items={sites}
-                onChange={handleSiteChange}
-                isOpen={popup}
-                onClick={() => setPopup(!popup)}
-              />
-              <div className="c-form-combo--inline o-layout__item u-width-3/4">
-                <div className="c-form-combo__cell">
-                  <input
-                    type="text"
-                    name="searchTerm"
-                    className="c-form-combo__input c-form-input"
-                    placeholder="Search for your next course"
-                    id="f-combo"
-                    value={searchTerm}
-                    onChange={event => setSearchTerm(event.target.value)}
-                  />
-                </div>
-                <div className="c-form-combo__cell">
-                  <Link
-                    to={{
-                      pathname: `/courses`,
-                      search: `?searchTerm=${searchTerm}&site=${siteId}`
-                    }}
-                    className="c-btn c-btn--primary"
-                  >
-                    Search
-                  </Link>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </fieldset>
-      </form>
+      <FormBuilder json={FormJSON} onSubmit={handleSubmit} />
     </div>
   );
 }
