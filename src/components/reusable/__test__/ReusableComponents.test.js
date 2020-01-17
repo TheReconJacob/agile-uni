66b0f71ad85e320412a520651f986faaa8023860
jack272hughes <jack272hughes@gmail.com>
2020-01-17 11:06:01 +0000

    Add default switch case
    Added default switch case to please the ESLINT gods
    Moved the const down forESLINT gods as well

--- src/components/reusable/__test__/ReusableComponents.test.js
+++ src/components/reusable/__test__/ReusableComponents.test.js
@@ -1,7 +1,7 @@
-const testJson = require("../../../testData/ReusableComponentTest");
-
 import reusableComponents from "./ReusableComponents";
 import { cleanup, render } from "@testing-library/react";
+
+const testJson = require("../../../testData/ReusableComponentTest");
 
 afterEach(cleanup);
 
@@ -9,21 +9,27 @@ function renderable(component) {
   switch (component) {
     case "Button":
       return ["Test", render(reusableComponents[component].expectedDom)];
+
     case "Modal":
       return [
         "lorem ipsum",
         render(reusableComponents[component].expectedDom.props.textToDisplay)
       ];
+
     case "Select":
       return [
         "Test Label",
         render(reusableComponents[component].expectedDom.props.labelText)
       ];
+
     case "RichText":
       return [
         "rich-text",
         render(reusableComponents[component].expectedDom.props.label)
       ];
+
+    default:
+      console.log(`Couldn't find component: "${component}"`);
   }
 }
 
